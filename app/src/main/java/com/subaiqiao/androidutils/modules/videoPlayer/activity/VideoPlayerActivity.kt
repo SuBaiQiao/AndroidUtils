ad6af35dd89d13ebe55a28ebe670d99d59a946b2
subaiqiao <1294749811@qq.com>
2024-06-20 21:02:03 +0800

    配置视频播放器

--- app/src/main/java/com/subaiqiao/androidutils/modules/videoPlayer/activity/VideoPlayerActivity.kt
+++ app/src/main/java/com/subaiqiao/androidutils/modules/videoPlayer/activity/VideoPlayerActivity.kt
@@ -32,11 +32,11 @@ class VideoPlayerActivity : ComponentActivity() {
         init()
     }
 
-    fun init() {
+    private fun init() {
         videoPlayer = findViewById (R.id.video_player)
 //        val source = "http://devimages.apple.com.edgekey.net/streaming/examples/bipbop_4x3/gear3/prog_index.m3u8"
         val source = "https://vip.ffzy-online.com/20221029/5952_80bc1f98/2000k/hls/mixed.m3u8"
-        videoPlayer.setUp(source, true, "测试视频");
+        videoPlayer.setUp(source, true, "测试视频")
         val imageView = ImageView(this)
         imageView.setScaleType(ImageView.ScaleType.CENTER_CROP)
         imageView.setImageResource(R.mipmap.xxx1)
@@ -46,14 +46,19 @@ class VideoPlayerActivity : ComponentActivity() {
         //设置返回键
         videoPlayer.backButton.setVisibility(View.VISIBLE)
         val orientationUtils = OrientationUtils(this, videoPlayer)
+        //设置全屏按键功能,这是使用的是选择屏幕，而不是全屏
         videoPlayer.fullscreenButton.setOnClickListener {
+            // ------- ！！！如果不需要旋转屏幕，可以不调用！！！-------
+            // 不需要屏幕旋转，还需要设置 setNeedOrientationUtils(false)
             orientationUtils.resolveByClick();
         }
+        //是否可以滑动调整
         videoPlayer.setIsTouchWiget(true)
+        //设置返回按键功能
         videoPlayer.backButton.setOnClickListener {
             onBackPressed()
         }
-
+        //不需要屏幕旋转
         videoPlayer.isNeedOrientationUtils = false
         videoPlayer.startPlayLogic()
     }
@@ -76,6 +81,11 @@ class VideoPlayerActivity : ComponentActivity() {
 
     @Deprecated("Deprecated in Java")
     override fun onBackPressed() {
+        ///       不需要回归竖屏
+//        if (orientationUtils.getScreenType() == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+//            videoPlayer.getFullscreenButton().performClick();
+//            return;
+//        }
         //释放所有
         videoPlayer.setVideoAllCallBack(null)
         super.onBackPressed()
